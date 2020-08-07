46dc74f84f2e8e7740c17c51cb159f0466ee470f
Ali Kodera <devestatoor@gmail.com>
2020-08-07 02:18:41 +0200

    bugfix

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", indexRoutes);
 
-app.listen(process.env.port || 3000, process.env.IP, () => {
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log("start");
 });
